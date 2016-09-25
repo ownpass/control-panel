df1af95013a6d40c6f7941326f9b75638a194240
Yassine Elouazzani <yassine@elouazzani.nl>
2016-09-26 00:08:27 +0200

    add the router module as provider in the main module

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,17 @@ import { AppComponent }   from './app.component';
 import { LoginFormComponent }   from './login-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders }  from './app.routing';
+import { PageNotFoundComponent} from './page-not-found.component';
+
 
 //enableProdMode();
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpModule ],
-  declarations: [ AppComponent, LoginFormComponent],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, routing ],
+  declarations: [ AppComponent, LoginFormComponent, PageNotFoundComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [appRoutingProviders],
 })
 
 
---
+++ app/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h2>404</h2>
+    <p>You must be lost bro!</p>
+  `,
+})
+
+export class PageNotFoundComponent { }

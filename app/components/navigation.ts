4c05ae4c2eb7d8c560009b275b6fe3a50a88d26b
Yassine Elouazzani <yassine@elouazzani.nl>
2016-09-29 00:52:38 +0200

    Added simple logout functionality

--- app/components/navigation.ts
+++ app/components/navigation.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OAuth } from '../services/oauth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-navigation',
@@ -13,9 +15,19 @@ import { Component } from '@angular/core';
       <li>
         <a routerLink="/fertert" routerLinkActive="active">404</a>
       </li>
+      <li>
+        <a (click)="logout($event)" href="#">Logout</a>
+      </li>
     </ul>
   `,
   styleUrls: ['app/components/navigation.css']
 })
 
-export class NavigationComponent {}
+export class NavigationComponent {
+  constructor(private oauth: OAuth, private router: Router) {}
+  public logout = (event: Event) => {
+    event.preventDefault();
+    this.oauth.removeToken();
+    this.router.navigateByUrl('login');
+  }
+}

e93eb203e9b33c7aac1847c4f87964561ccbba38
Yassine Elouazzani <yassine@elouazzani.nl>
2016-10-14 01:19:47 +0200

    Passwords page - toggle list & grid view

--- app/components/vault/vault.ts
+++ app/components/vault/vault.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Vault} from '../../services/vault'
+import {LS} from '../../services/localstorage';
 
 @Component({
     selector: 'ownpass-vault',
@@ -10,13 +11,27 @@ import {Vault} from '../../services/vault'
 export class VaultComponent {
     public result;
     public vault;
+    private localStorageKey:string = 'ownpassView';
 
-    constructor(private vaultService: Vault) {
+    public getView = () => {
+        let ls = this.ls.get(this.localStorageKey);
+        return ls === 'grid';
+    }
+
+    public isGrid:boolean = this.getView();
+
+    constructor(private vaultService: Vault, private ls: LS) {
         this.result = this.vaultService.get()
             .subscribe(
                 response => {
                     this.vault = response._embedded.user_credential;
                     this.result = response;
                 });
+    }
+
+    public toggleView = (event: Event) => {
+        event.preventDefault();
+        this.isGrid = !this.isGrid;
+        this.ls.set(this.localStorageKey, this.isGrid ? 'grid' : 'list');
     }
 }
